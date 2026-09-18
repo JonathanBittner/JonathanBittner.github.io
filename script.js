3d0196f5622f990266da156e4a628681db5fb47d
JonathanBittner <jbittner@gmail.com>
2026-09-18 01:24:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -190,13 +190,13 @@ map.on('style.load', () => {
   url: "geojson-source/interstates-primary/transformed/i-80s-major-transformed.geojson"
   }
   */
-    map.setPaintProperty('us-90s-major', 'line-color', colors.signed.major.mainline[9],
-      'line-width', defaultLineWidth); 
-    map.setPaintProperty('us-80s-major', 'line-color', colors.signed.major.mainline[8],
-      'line-width', defaultLineWidth); 
-   map.setPaintProperty('us-70s-major', 'line-color', colors.signed.major.mainline[7],
+    map.setPaintProperty('us-90s-major', 'line-color', colors.signed.major.mainline[9]);
+    map.setPaintProperty('us-90s-major', 'line-width', defaultLineWidth); 
+    map.setPaintProperty('us-80s-major', 'line-color', colors.signed.major.mainline[8])
+    map.setPaintProperty('us-80s-major', 'line-width', defaultLineWidth); 
+    map.setPaintProperty('us-70s-major', 'line-color', colors.signed.major.mainline[7],
      'line-width', defaultLineWidth);
-   map.setPaintProperty('us-00s-major', 'line-color', colors.signed.major.mainline[0],
+    map.setPaintProperty('us-00s-major', 'line-color', colors.signed.major.mainline[0],
       'line-width', defaultLineWidth);
 
    map.setPaintProperty('us-00s-primary', 'line-color', colors.signed.major.mainline[0],
