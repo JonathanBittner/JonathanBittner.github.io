8a305ec09c503ac8bcc8f184798a16eb30d2ec70
JonathanBittner <jbittner@gmail.com>
2026-09-17 22:31:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ mapboxgl.accessToken =
 const defaultLineWidth = 2
 
 //const layerList = ['triplex-interstates-0','triplex-interstates-1','triplex-interstates-2','duplex-interstates-0','duplex-interstates-1','i-90s-major','i-80s-major','i-70s-major','i-60s-major','i-50s-major','i-40s-major','i-30s-major','i-20s-major','i-10s-major','i-00s-major','i-90s-primary','i-80s-primary','i-70s-primary','i-60s-primary','i-50s-primary','i-40s-primary','i-30s-primary','i-20s-primary','i-10s-primary','i-00s-primary','auxiliary-interstates','unsigned-interstates','business-interstates','unsigned-business-interstates','future-interstates']
-const layerList = ['us-90s-major', 'us-80s-major', 'us-70s-major'];
+const layerList = ['us-90s-major', 'us-80s-major', 'us-70s-major', 'us-00s-major', 'us-00s-primary'];
 ////////////////////////DEFINING COLOR PALETTE//////////////////////////
 
 //Base colors, slightly modified from perceptually uniform colors
@@ -436,6 +436,9 @@ map.on('load', function () {
     }
   },"i-10s-major"); */
 
+      map.setPaintProperty('us-00s-major', 'line-color', colors.signed.major.mainline[0],
+      'line-width', defaultLineWidth);
+
   ////////////////OTHER PRIMARY INTERSTATES////////////////////////////////////////////////
   
  /* map.addLayer({
@@ -588,7 +591,8 @@ map.on('load', function () {
     }
   },"i-10s-primary");
   */
-  
+      map.setPaintProperty('us-00s-primary', 'line-color', colors.signed.major.mainline[0],
+      'line-width', defaultLineWidth);
   ////////////////AUXILIARY INTERSTATES////////////////////////////////////////////////
   
 /*  map.addLayer({
