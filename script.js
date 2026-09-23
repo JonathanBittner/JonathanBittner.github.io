3fffeab41263b8bf745b21e5ab052fd31fe7628a
JonathanBittner <jbittner@gmail.com>
2026-09-23 02:02:38 -0400

    changed layer order

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ map.on('style.load', () => {
     },
     filter: ['all', ["==", ["get", "route0_status"], "signed"], ["==", ["get", "route1_status"], "signed"], ["==", ["get", "route2_status"], "signed"], ["==", ["get", "route0_role"], "mainline"], ["==", ["get", "route1_role"], "mainline"],
 	["==", ["get", "route2_role"], "mainline"]]
-    });
+    }, 'us-00s-major');
 	
 	map.addLayer({
 		id: "triplex-us-1",
