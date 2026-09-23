94f91935ff2b5db171fa1cdffe6cd45704805373
JonathanBittner <jbittner@gmail.com>
2026-09-23 04:36:26 -0400

    missing brace

--- script.js
+++ script.js
@@ -235,7 +235,8 @@ map.on('style.load', () => {
     },
     filter: ['all', ["==", ["get", "route0_status"], "signed"], ["==", ["get", "route1_status"], "signed"], ["==", ["get", "route2_status"], "signed"], ["==", ["get", "route0_role"], "mainline"], ["==", ["get", "route1_role"], "mainline"],
 	["==", ["get", "route2_role"], "mainline"]],
-	slot: "middle");
+	slot: "middle"
+	});
 	
 	map.addLayer({
 		id: "triplex-us-1",
